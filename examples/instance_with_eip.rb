0ec8de618c257b884d45a40176e136a8c0232215
cgorski <cgorski@cgorski.org>
2013-02-19 19:41:58 +0000

    Update instance with EIP example

--- examples/instance_with_eip.rb
+++ examples/instance_with_eip.rb
@@ -2,10 +2,10 @@
 
 require 'cf_factory'
 
-cf = CfMain.new("Playground")
+cf = CfFactory::CfMain.new("Playground")
 ##########################
 
-mapping = CfMapping.new("Region2AmiMapping","AMI", {
+mapping = CfFactory::CfMapping.new("Region2AmiMapping","AMI", {
     "us-east-1" => "ami-c6699baf",
     "us-west-2" => "ami-52ff7262",
     "us-west-1" => "ami-3bcc9e7e",
@@ -16,25 +16,25 @@ mapping = CfMapping.new("Region2AmiMapping","AMI", {
   })
 cf.add_mapping(mapping)
 ami = mapping.map_from_region()
-instance = CfEc2Instance.new("MyInstance", ami, "t1.micro")
+instance = CfFactory::CfEc2Instance.new("MyInstance", ami, "t1.micro")
 cf.add_resource(instance)
-eip = CfEip.new("MyEip",instance)
+eip = CfFactory::CfEip.new("MyEip",instance)
 cf.add_resource(eip)
-instance_ip_assoc = CfEipAssociation.new("MyEipAllocation", eip, instance, nil)
+instance_ip_assoc = CfFactory::CfEipAssociation.new("MyEipAllocation", eip, instance, nil)
 cf.add_resource(instance_ip_assoc)
 
-eip_output = CfOutput.new("Eip", "Id of the EIP", eip.generate_ref())
+eip_output = CfFactory::CfOutput.new("Eip", "Id of the EIP", eip.generate_ref())
 cf.add_output(eip_output)
 #eip_ip_output = CfOutput.new("Eip", "Id of the EIP", eip.retrieve_attribute("AllocationId")) only works for VPC
 #cf.add_output(eip_ip_output)
-assoc_output = CfOutput.new("Assoc", "Id of the Assoc", instance_ip_assoc.generate_ref())
+assoc_output = CfFactory::CfOutput.new("Assoc", "Id of the Assoc", instance_ip_assoc.generate_ref())
 cf.add_output(assoc_output)
 ##########################
 cf_json = cf.generate 
 puts cf_json
 
 config_options = YAML.load_file("aws_config.yml")
-validator = TemplateValidation.new(cf_json, config_options)
-validator.validate()
-validator.apply()
+#validator = CfFactory::TemplateValidation.new(cf_json, config_options)
+#validator.validate()
+#validator.apply()
 
